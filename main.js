22fb9c9403bf6d0f4a68133e706a1975d745cfd0
brandonPerry-Co <Brandonperrymba@gmail.com>
2021-01-25 16:12:45 -0600

    Piglatin GUI

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const word = document.querySelector("text")
 // brings in the assert module for unit testing
 const assert = require('assert');
 // brings in the readline module to access the command line
